3ab73d4d2a767947cc29b9a61a88c7e1d44cec67
CARLOS-G <110358830+CARLOS-V-G@users.noreply.github.com>
2023-06-14 22:35:53 -0300

    update formulario.js

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -1,96 +1,120 @@
-const $ = (id) => document.getElementById(id)
-const urlBase = "http://localhost:3031/api/movies/"
+const $ = (id) => document.getElementById(id);
+const urlBase = "http://localhost:3031/api/movies/";
 
-window.onload = async() => {
-    let query = new URLSearchParams(location.search)
-    let id = query.has('id') && query.get('id')
-    const qs = (search) => {
-        return document.querySelector(search)
-    }
+window.onload = async () => {
+  let query = new URLSearchParams(location.search);
+  let id = query.has('id') && query.get('id');
+  const qs = (search) => {
+    return document.querySelector(search);
+  };
 
-    try {
+  try {
+    let response = await fetch(urlBase + id);
+    let peliculas = await response.json();
+    let { title, rating, awards, length: duracion, release_date, genre_id } =
+      peliculas.data;
+    let releaseDateFormatted = moment(release_date).format('YYYY-MM-DD');
 
-        let response = await fetch(urlBase + id)
-        let peliculas = await response.json()
-        let {title, rating, awards, length:duracion, release_date,genre_id} = peliculas.data
-        let releaseDateFormatted = moment(release_date).format('YYYY-MM-DD');
+    $('title').value = title;
+    $('rating').value = rating;
+    $('awards').value = awards;
+    $('length').value = duracion;
+    $('genre_id').value = genre_id;
+    $('release_date').value = releaseDateFormatted;
+  } catch (error) {
+    console.error(error);
+  }
 
-        $('title').value = title;
-        $('rating').value = rating;
-        $('awards').value = awards;
-        $('length').value = duracion;
-        $('genre_id').value = genre_id;
-        $('release_date').value = releaseDateFormatted;
-
-
-        
-    } catch (error) {
-        console.error
-    }
-  document.querySelector('.formulario').addEventListener('submit', async (e) => {
+  document.querySelector('.boton-editar').addEventListener('click', async (e) => {
     e.preventDefault();
 
     try {
-        let response;
-        let successMessage;
+      let response = await fetch(urlBase + 'update/' + id, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title: $('title').value,
+          rating: $('rating').value,
+          awards: $('awards').value,
+          length: $('length').value,
+          genre_id: $('genre_id').value,
+          release_date: $('release_date').value,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-        if ((id && !erase.checked)) {
-            response = await fetch(urlBase + 'update/' + id, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    title: $('title').value,
-                    rating: $('rating').value,
-                    awards: $('awards').value,
-                    length: $('length').value,
-                    genre_id:$('genre_id').value,
-                    release_date: $('release_date').value
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            successMessage = 'Película editada con éxito';
-        } else if (!id) {
-            response = await fetch(urlBase + 'create', {
-                method: 'POST',
-                body: JSON.stringify({
-                    title: $('title').value,
-                    rating: $('rating').value,
-                    awards: $('awards').value,
-                    length: $('length').value,
-                    genre_id:$('genre_id').value,
-                    release_date: $('release_date').value
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            successMessage = 'Película creada con éxito'
-        }else if (erase.checked){
-            response = await fetch(urlBase +'delete/'+id, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+      let successMessage = 'Película editada con éxito';
 
-            successMessage = 'Película eliminada con éxito';
-        }else{
+      let body = qs('body');
+      const edit = document.createElement('h2');
+      edit.innerText = successMessage;
+      body.appendChild(edit);
 
-        }
-
-        let body = qs('body')
-        const edit = document.createElement('h2')
-        edit.innerText = successMessage;
-        body.appendChild(edit);
-
-        let result = await response.json();
-        console.log(result);
-
+      let result = await response.json();
+      console.log(result);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-});
-    
+  });
 
-}  
+  document.querySelector('.boton-crear').addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await fetch(urlBase + 'create', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: $('title').value,
+          rating: $('rating').value,
+          awards: $('awards').value,
+          length: $('length').value,
+          genre_id: $('genre_id').value,
+          release_date: $('release_date').value,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      let successMessage = 'Película creada con éxito';
+
+      let body = qs('body');
+      const edit = document.createElement('h2');
+      edit.innerText = successMessage;
+      body.appendChild(edit);
+
+      let result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  document.querySelector('.boton-eliminar').addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await fetch(urlBase + 'delete/' + id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      let successMessage = 'Película eliminada con éxito';
+
+      let body = qs('body');
+      const edit = document.createElement('h2');
+      edit.innerText = successMessage;
+      body.appendChild(edit);
+
+      let result = await response.json();
+      console.log(result);
+
+      window.location.href = 'home.html'; // Redireccionar a home.html
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
